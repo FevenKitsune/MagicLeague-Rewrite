44b74a56e8a8a60c697d6cd9721a2fc4bcf0c03f
Feven Kitsune <fevenkitsune@gmail.com>
2019-06-29 13:03:17 -0400

    + basic client setup

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("Hello world!")
+import discord
+from discord.ext import commands
+from prefix import get_prefix
+
+client = commands.Bot(
+    description="MagicLeague-RW",
+    command_prefix=get_prefix
+)
---
+++ src/prefix.py
@@ -0,0 +1,2 @@
+async def get_prefix(bot, message):
+    return "m^"
