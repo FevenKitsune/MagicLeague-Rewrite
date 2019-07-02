cd6779ad8b240751cdf3fbd92a7970ef002b6c96
Feven Kitsune <fevenkitsune@gmail.com>
2019-07-02 11:34:52 -0400

    + basic CRUD operations

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import discord
 from discord.ext import commands
-from prefix import get_prefix
+from google.cloud import datastore
+from src.util.prefix import get_prefix, set_prefix
 
+ds = datastore.Client()
 client = commands.Bot(
     description="MagicLeague-RW",
     command_prefix=get_prefix
 )
+
---
+++ src/util/prefix.py
@@ -0,0 +1,18 @@
+from google.cloud import datastore
+from src.main import ds
+
+
+async def get_prefix(bot, message):
+    key = ds.key('prefix', str(message.guild.id))
+    task = ds.get(key)
+    if not task:
+        return "m^"
+    return str(task['prefix'])
+
+
+async def set_prefix(ctx, prefix):
+    key = ds.key('prefix', str(ctx.guild.id))
+    task = ds.Entity(key=key)
+    task['prefix'] = str(prefix)
+    ds.put(task)
+    return
